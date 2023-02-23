21731e012ac7dc41e1b30cb0fe47ccf2aa136ceb
Moritz Gottschling <moritz@giantswarm.io>
2023-02-23 08:14:42 +0100

    Fix yaml marshaling (#4)
    * Fix yaml marshaling
    
    * Update changelog

--- pkg/marshal/yaml.go
+++ pkg/marshal/yaml.go
@@ -1,14 +1,38 @@
 package marshal
 
 import (
+	"bytes"
+
 	"github.com/giantswarm/microerror"
 	"gopkg.in/yaml.v3"
+	sigsYaml "sigs.k8s.io/yaml"
 )
 
 func ToYaml(data interface{}) ([]byte, error) {
-	marshalled, err := yaml.Marshal(data)
+	convertedData, err := convertJSONNumbers(data)
+	if err != nil {
+		return nil, microerror.Maskf(marshalError, "Error during YAML marshaling:\n%s", err)
+	}
+
+	var buf bytes.Buffer
+	enc := yaml.NewEncoder(&buf)
+	enc.SetIndent(2)
+	err = enc.Encode(convertedData)
 	if err != nil {
 		return nil, microerror.Maskf(marshalError, "Error marshalling to YAML:\n%s", err)
 	}
+	marshalled := buf.Bytes()
 	return marshalled, nil
+}
+
+// marshals to YAML and unmarshals back to interface{} to convert json.Number appropriately
+func convertJSONNumbers(data interface{}) (interface{}, error) {
+	b, err := sigsYaml.Marshal(data)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+	var converted interface{}
+	err = sigsYaml.Unmarshal(b, &converted)
+	return converted, microerror.Mask(err)
+
 }
--- pkg/marshal/yaml_test.go
+++ pkg/marshal/yaml_test.go
@@ -1,9 +1,81 @@
 package marshal
 
 import (
+	"encoding/json"
 	"strings"
 	"testing"
+
+	"github.com/google/go-cmp/cmp"
 )
+
+func TestToYaml(t *testing.T) {
+	testCases := []struct {
+		name string
+		data interface{}
+		want string
+	}{
+		{
+			name: "case 0: simple",
+			data: map[string]interface{}{
+				"foo": "bar",
+			},
+			want: "foo: bar\n",
+		},
+		{
+			name: "case 1: long string with possible line break",
+			data: map[string]interface{}{
+				"test": "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa bbb",
+			},
+			want: "test: aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa bbb\n",
+		},
+		{
+			name: "case 2: integer",
+			data: map[string]interface{}{
+				"test": 1,
+			},
+			want: "test: 1\n",
+		},
+		{
+			name: "case 3: float",
+			data: map[string]interface{}{
+				"test": 1.1,
+			},
+			want: "test: 1.1\n",
+		},
+		{
+			name: "case 2: json.Number",
+			data: map[string]interface{}{
+				"test": json.Number("1"),
+			},
+			want: "test: 1\n",
+		},
+		{
+			name: "case 5: nested object",
+			data: map[string]interface{}{
+				"test": map[string]interface{}{
+					"foo": "bar",
+				},
+			},
+			want: `test:
+  foo: bar
+`,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			got, err := ToYaml(tc.data)
+			if err != nil {
+				t.Fatalf("unexpected error: %v", err)
+			}
+
+			if string(got) != tc.want {
+				t.Fatalf("unexpected result: %s", cmp.Diff(string(got), tc.want))
+			}
+		})
+	}
+
+}
 
 func TestToYamlWithLongStrings(t *testing.T) {
 	data := map[string]interface{}{
