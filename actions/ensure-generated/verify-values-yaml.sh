561c1006786a367baea662ba40a4e2b5d972d735
Moritz Gottschling <moritz@giantswarm.io>
2023-03-09 11:09:11 +0100

    Add github action

---
+++ actions/ensure-generated/verify-values-yaml.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+VALUES_SCHEMA=$(find ./helm -maxdepth 2 -name values.schema.json)
+VALUES=$(find ./helm -maxdepth 2 -name values.yaml)
+
+if diff --unified ${VALUES} <(helm-values-gen ${VALUES_SCHEMA}) ; then
+  echo "values.yaml is up to date"
+  exit 0
+else
+  echo "values.yaml is out of date"
+  exit 1
+fi
