6cc50b9f9475e7a308f69fb82f2629eeb99ddb11
Moritz Gottschling <moritz@giantswarm.io>
2023-03-16 11:58:41 +0100

    Trigger update action workflow when "Create Release PR" is completed

---
+++ .github/workflows/find_release_branch.js
@@ -0,0 +1,5 @@
+const regex = "^Release v((0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?)$"
+
+
+
+// gh search prs --repo giantswarm/helm-values-gen --state open --json title,number
