285b8a8315809b667793bf7aa32e11d8a7df623a
Marian Steinbach <marian@giantswarm.io>
2025-02-12 12:44:09 +0100

    Fix shell syntax to prevent globbing and word splitting

--- actions/ensure-generated/verify-values-yaml.sh
+++ actions/ensure-generated/verify-values-yaml.sh
@@ -3,7 +3,7 @@
 VALUES_SCHEMA=$(find ./helm -maxdepth 2 -name values.schema.json)
 VALUES=$(find ./helm -maxdepth 2 -name values.yaml)
 
-if diff --unified ${VALUES} <(helm-values-gen ${VALUES_SCHEMA}) ; then
+if diff --unified "${VALUES}" <(helm-values-gen "${VALUES_SCHEMA}") ; then
   echo "values.yaml is up to date"
   exit 0
 else
